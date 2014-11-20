7011711c490cd9d70a004a3710c748c3ed74b208
angryhussord <angryhussord@outlook.com>
2014-11-20 10:31:01 -0800

    Update package for 0.638 release and added icon file

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'obs'
 $installerType = 'EXE'
-$url = 'https://github.com/jp9000/OBS/releases/download/0.637b/OBS_0_637b_Installer.exe'
+$url = 'https://github.com/jp9000/OBS/releases/download/0.638b/OBS_0_638b_Installer.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
