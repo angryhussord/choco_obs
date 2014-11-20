e31713d56f7745b3359ba75104d14069e9a6838c
angryhussord <angryhussord@outlook.com>
2014-11-20 10:27:51 -0800

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'obs'
+$installerType = 'EXE'
+$url = 'https://github.com/jp9000/OBS/releases/download/0.637b/OBS_0_637b_Installer.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
