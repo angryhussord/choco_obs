5940cd52174d9eb46722f5b8cd3b0cdc6574ae40
Patrick Hufford <pahuff@microsoft.com>
2015-10-21 03:33:17 -0700

    Updating for 0.656

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'obs'
 $installerType = 'EXE'
-$url = 'https://github.com/jp9000/OBS/releases/download/0.655b/OBS_0_655b_Installer.exe'
+$url = 'https://github.com/jp9000/OBS/releases/download/0.656b/OBS_0_656b_Installer.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
